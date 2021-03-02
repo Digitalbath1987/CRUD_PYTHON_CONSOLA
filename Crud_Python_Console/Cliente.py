39a204ba5cfb930876b7d060d74cd7a38ab3a98e
mario rosales <mario.rosales02@inacapmail.cl>
2021-03-02 18:24:22 -0300

    se crea funcion conector

--- Crud_Python_Console/Cliente.py
+++ Crud_Python_Console/Cliente.py
@@ -1,15 +1,21 @@
 import pyodbc
 class Cliente:
-
-    def Obtener():
-        #Conexion a SQL Server
-        try:
-            Conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+    
+    #Conexion a SQL Server CONEXION
+    def Conn():
+        Conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
                       'Server=MARIOROSALES\DIGITALDB;'
                       'Database=operaciones;'
                       'UID=sa;'
                       'PWD=123456;')
-            Cursor  = Conn.cursor();
+        return Conn
+
+
+        #Conexion a SQL Server READ
+    def Obtener():
+        Conn    = Cliente.Conn()
+        Cursor  = Conn.cursor();
+        try:
             Cursor.execute('SELECT * FROM CLIENTE with(nolock)')
             for row in Cursor:
                 print(row)
@@ -18,14 +24,10 @@ class Cliente:
         finally:
             Conn.close()
 
+        #Conexion a SQL Server CREATE
     def ADD(NOMBRE,RUT,TELEFONO_1,TELEFONO_2,ID_CIUDAD,DIRECCION,EMAIL,ID_COMERCIAL,ESTADO,NOMBRE_CONTACTO,COD_POSTAL,GIRO):
-        #Conexion a SQL Server
         try:
-            Conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-                      'Server=MARIOROSALES\DIGITALDB;'
-                      'Database=operaciones;'
-                      'UID=sa;'
-                      'PWD=123456;')
+            Conn    = Cliente.Conn()
             Cursor  = Conn.cursor();
             Query   = ('INSERT INTO CLIENTE'
                        '(NOMBRE'
